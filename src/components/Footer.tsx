9f038c461b8e81678209bc2bfcf1bd12d0587e74
flexteck <flexteckse@gmail.com>
2025-09-28 01:41:20 +0100

    corrected the right section of the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -27,15 +27,16 @@ export default function Footer() {
         {/* Right Section */}
         <div className="flex items-center gap-2 text-sm">
           <span>Curated by</span>
-          <div className="h-6 w-6 flex items-center justify-center">
+          <div className="flex items-center gap-2 text-sm">
+            <span>Curated by</span>
             <Image
               src="/icons/mobbin_footer.svg"
               alt="Mobbin Logo"
-              width={20}
-              height={20}
+              width={32}
+              height={32}
               className="object-contain"
             />
-          </div>
+        </div>
         </div>
       </div>
     </footer>
